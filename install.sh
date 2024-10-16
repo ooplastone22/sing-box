99aa2ddeb3dacf2432e5b18de04441a68574151d
Aijun Wang <143001488+ooplastone22@users.noreply.github.com>
2024-10-16 16:11:45 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -427,11 +427,11 @@ main() {
     # create a config
     add tuic 28090 4691d36a-b953-4935-bad9-3d5f00746a6b
     
-    add hy2 38090 4691d36a-b953-4935-bad9-3d5f00746a6b
+   # add hy2 38090 4691d36a-b953-4935-bad9-3d5f00746a6b
    
-   # add ss 48090 2kge73478g aes-256-gcm
+    add ss 48090 2kge73478g aes-256-gcm
     
-   # add rh2 18090 4691d36a-b953-4935-bad9-3d5f00746a6b www.cloudflare.com
+    add rh2 18090 4691d36a-b953-4935-bad9-3d5f00746a6b www.cloudflare.com
     # remove tmp dir and exit.
     exit_and_del_tmpdir ok
 }
