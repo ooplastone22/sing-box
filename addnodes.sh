b7ea12b9d8d5c60e5b5707700b4cdcbbf053b214
Aijun Wang <143001488+ooplastone22@users.noreply.github.com>
2024-10-16 17:01:27 +0800

    Add files via upload

---
+++ addnodes.sh
@@ -0,0 +1,4 @@
+sudo sing-box add reality 18090 4691d36a-b953-4935-bad9-3d5f00746a6b www.cloudflare.com
+sudo sing-box add tuic 28090 4691d36a-b953-4935-bad9-3d5f00746a6b
+sudo sing-box add hy2 38090 4691d36a-b953-4935-bad9-3d5f00746a6b
+sudo sing-box add ss 48090 4691d36a-b953-4935-bad9-3d5f00746a6b aes-256-gcm
