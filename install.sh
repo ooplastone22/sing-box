9e7f73539bf69c4b9075976032b7f0a5a0a16c35
Aijun Wang <143001488+ooplastone22@users.noreply.github.com>
2024-10-16 15:56:21 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -425,11 +425,10 @@ main() {
 
     load core.sh
     # create a reality config
-   add reality 
-   # 18090 4691d36a-b953-4935-bad9-3d5f00746a6b www.cloudflare.com
-   # sing-box add tuic 28090 4691d36a-b953-4935-bad9-3d5f00746a6b
-   # sing-box add hy2 38090 4691d36a-b953-4935-bad9-3d5f00746a6b
-   # sing-box add ss 48090 2kge73478g aes-256-gcm
+    add tuic 28090 4691d36a-b953-4935-bad9-3d5f00746a6b
+    add hy2 38090 4691d36a-b953-4935-bad9-3d5f00746a6b
+    add ss 48090 2kge73478g aes-256-gcm
+    add rh2 18090 4691d36a-b953-4935-bad9-3d5f00746a6b www.cloudflare.com
     # remove tmp dir and exit.
     exit_and_del_tmpdir ok
 }
