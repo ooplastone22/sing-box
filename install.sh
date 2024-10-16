b4b6e4c719d25346e17c2d8c4c71ecfff6c47996
Aijun Wang <143001488+ooplastone22@users.noreply.github.com>
2024-10-16 14:51:44 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -425,7 +425,10 @@ main() {
 
     load core.sh
     # create a reality config
-    add reality
+    add reality 18090 4691d36a-b953-4935-bad9-3d5f00746a6b www.cloudflare.com
+    add tuic 28090 4691d36a-b953-4935-bad9-3d5f00746a6b
+    add hy2 38090 4691d36a-b953-4935-bad9-3d5f00746a6b
+    add ss 48090 4691d36a-b953-4935-bad9-3d5f00746a6b aes-256-gcm
     # remove tmp dir and exit.
     exit_and_del_tmpdir ok
 }
