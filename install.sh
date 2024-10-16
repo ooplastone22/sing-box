b477da768b31e15bfd15a0d34049e00ede1de068
Aijun Wang <143001488+ooplastone22@users.noreply.github.com>
2024-10-16 16:04:08 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -424,14 +424,14 @@ main() {
     mkdir -p $is_conf_dir
 
     load core.sh
-    # create a reality config
+    # create a config
     add tuic 28090 4691d36a-b953-4935-bad9-3d5f00746a6b
-    wait
-    add hy2 38090 4691d36a-b953-4935-bad9-3d5f00746a6b
-    wait
-    add ss 48090 2kge73478g aes-256-gcm
-    wait
-    add rh2 18090 4691d36a-b953-4935-bad9-3d5f00746a6b www.cloudflare.com
+    
+   # add hy2 38090 4691d36a-b953-4935-bad9-3d5f00746a6b
+   
+   # add ss 48090 2kge73478g aes-256-gcm
+    
+   # add rh2 18090 4691d36a-b953-4935-bad9-3d5f00746a6b www.cloudflare.com
     # remove tmp dir and exit.
     exit_and_del_tmpdir ok
 }
