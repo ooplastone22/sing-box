62a56179d468a3232ea07bb05039b7f691472b63
Aijun Wang <143001488+ooplastone22@users.noreply.github.com>
2026-01-15 09:16:46 +0800

    Uncomment sing-box commands in install.sh

--- install.sh
+++ install.sh
@@ -427,10 +427,10 @@ main() {
     load core.sh
     # create a reality config
         add tcp 18090 4691d36a-b953-4935-bad9-3d5f00746a6b
-    #    sing-box add tuic 28090 4691d36a-b953-4935-bad9-3d5f00746a6b
+        sing-box add tuic 28090 4691d36a-b953-4935-bad9-3d5f00746a6b
     #    sing-box add hy2 38090 4691d36a-b953-4935-bad9-3d5f00746a6b
         sing-box add reality 48090 4691d36a-b953-4935-bad9-3d5f00746a6b www.icloud.com
-        sing-box add ss 58090 4691d36a-b953-4935-bad9-3d5f00746a6b aes-256-gcm
+    #    sing-box add ss 58090 4691d36a-b953-4935-bad9-3d5f00746a6b aes-256-gcm
     #    sing-box add socks 28090 4691d36a 3d5f00746a6b
         sing-box bbr
     # remove tmp dir and exit.
